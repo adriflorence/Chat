0cfd8fb8e58579a45f2faecff7cadfc854494aae
Adri Forczek <forczekadri@gmail.com>
2020-09-01 23:43:30 +0100

    feature: extended setup

---
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatForm.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.c1.review.model;
+
+public class ChatForm {
+
+    private String messageText;
+    private String username;
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.udacity.jwdnd.c1.review.model;
+
+public class ChatMessage {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
